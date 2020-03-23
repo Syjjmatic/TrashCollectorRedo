2e33e260bb153bf35a45da048ca862d4185b2365
Andrew Curzon <andrew.w.curzon@gmail.com>
2020-03-23 10:17:38 -0500

    Added var roles to OnGetAsync method in RegisterModel class - no issues

--- TrashCollectorRedo/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ TrashCollectorRedo/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -74,6 +74,8 @@ namespace TrashCollectorRedo.Areas.Identity.Pages.Account
         {
             ReturnUrl = returnUrl;
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+            var roles = _roleManager.Roles;
+            Roles = new SelectList(roles, "Name", "Name");
         }
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
