c0bc299c8a8f3298c8a972b1d5281d34ee1a1bdc
Andrew Curzon <andrew.w.curzon@gmail.com>
2020-03-23 10:08:08 -0500

    init

---
+++ TrashCollectorRedo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TrashCollectorRedo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
