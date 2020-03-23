b10cbbdbc54ac24f9f297d21555ea8b806718951
Andrew Curzon <andrew.w.curzon@gmail.com>
2020-03-23 11:18:17 -0500

    Added properties to customer model

--- TrashCollectorRedo/Models/Customer.cs
+++ TrashCollectorRedo/Models/Customer.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,7 +12,20 @@ namespace TrashCollectorRedo.Models
     {
         public int CustomerId { get; set; }
         public string Name { get; set; }
+
+        [Display(Name="Pickup Day")]
+        public DayOfWeek PickupDay { get; set; }
+        
+        [Display(Name="One-time Pickup Day")]
         public DateTime OneTimePickup { get; set; }
+        
+        public int Balance { get; set; }
+        
+        [Display(Name = "Suspend Trash Start Date")]
+        public DateTime SuspendStart { get; set; }
+        
+        [Display(Name = "Suspend Trash End Date")]
+        public DateTime SuspendEnd { get; set; }
 
         [ForeignKey("IdentityUser")]
         public string IdentityUserId { get; set; }
